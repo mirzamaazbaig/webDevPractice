998d3ff9385e3cd5b1a41d9ea1dec91e466c6534
maazBaig <mirza.maazbaig@coreview.com>
2024-09-22 15:15:42 +0200

    Created password logic

--- Backend/9.2 Authentication Lv.2/index.js
+++ Backend/9.2 Authentication Lv.2/index.js
@@ -65,7 +65,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const loginPassword = req.body.password;
 
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
@@ -73,13 +73,20 @@ app.post("/login", async (req, res) => {
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      const storedPassword = user.password;
+      const storedHashedPassword = user.password;
 
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
+      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+        if (err) {
+          console.log("Error Comparing Password: ", err);
+        } else {
+          if (result) {
+            res.render("secrets.ejs");
+          } else {
+            res.send("Incorrect Password");
+          }
+        }
+      })
+
     } else {
       res.send("User not found");
     }
